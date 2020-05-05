67ccae0cd0c957344cddc9eafc0a836ea8b09c36
m0nkeywrench <59466775+m0nkeywrench@users.noreply.github.com>
2020-05-05 19:09:37 +0900

    generate create course page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,4 +3,10 @@ class CoursesController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "courses#index"
-  resources :courses, only: [:index]
+  resources :courses, only: [:index, :new, :create]
   resources :users,   only: [:show]
 end
