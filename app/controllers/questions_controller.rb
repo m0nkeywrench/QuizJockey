e1b534153640cd3009a9beddacce1912391a3e98
m0nkeywrench <59466775+m0nkeywrench@users.noreply.github.com>
2020-05-06 17:32:56 +0900

    generate question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,10 +4,18 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @course = Course.find(params[:course_id])
     @question = Question.new
   end
 
   def create
+    Question.create(question_params)
+    @course = Course.find(params[:course_id])
+    redirect_to new_course_question_path(params[:course_id])
   end
   
+  private
+  def question_params
+    params.require(:question).permit(:sentence, :answer, :wrong1, :wrong2, :wrong3, :commentary).merge(course_id: params[:course_id])
+  end
 end
