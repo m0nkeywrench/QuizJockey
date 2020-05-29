c6b61a31891e6ff5e290494f49ed93e152fe3ba5
m0nkeywrench <59466775+m0nkeywrench@users.noreply.github.com>
2020-05-29 21:25:31 +0900

    write method tests of course model

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -77,4 +77,77 @@ describe Course do
       expect(course).to eq(false)
     end
   end
+
+  describe "#get_course_list" do
+    before do
+      @user = FactoryBot.create(:user)
+      @available_course = FactoryBot.create(:course, private: false, user_id: @user.id)
+      FactoryBot.create(:question, course_id: @available_course.id)
+
+      @private_course = FactoryBot.create(:course, private: true, user_id: @user.id)
+      FactoryBot.create(:question, course_id: @private_course.id)
+
+      @making_course = FactoryBot.create(:course, private:true, user_id: @user.id)
+    end
+
+    it "問題数0のcourseがカットされること" do
+      expect(Course.get_course_list).to_not include(@making_course)
+    end
+
+    it "privateがtrueのコースがカットされること" do
+      expect(Course.get_course_list).to_not include(@private_course)
+    end
+
+    it "問題数が0ではないかつprivateがfalseのコースがカットされないこと" do
+      expect(Course.get_course_list).to include(@available_course)
+    end
+  end
+
+  describe "#paginate" do
+    before do # 作成時間降順で並ぶことを確かめるため、時間がかかる
+      @user = FactoryBot.create(:user)
+      @course_1st = FactoryBot.create(:course, user_id: @user.id)
+      sleep 1
+      @course_2nd = FactoryBot.create(:course, user_id: @user.id)
+      sleep 1
+      6.times do
+        FactoryBot.create(:course, user_id: @user.id)
+      end
+      sleep 1
+      @course_9th = FactoryBot.create(:course, user_id: @user.id)
+    end
+
+    it "9個courseがある場合、1ページ目には1番目に作成されたcourseは含まれない" do
+      expect(Course.paginate(1)).to_not include(@course_1st)
+    end
+
+    it "9個courseがある場合、1ページ目には2番目に作成されたcourseは含まれる" do
+      expect(Course.paginate(1)).to include(@course_2nd)
+    end
+
+    it "9個courseがある場合、1ページ目には9番目に作成されたcourseは含まれる" do
+      expect(Course.paginate(1)).to include(@course_9th)
+    end
+  end
+
+  describe "#search" do
+    before do
+      @user = FactoryBot.create(:user)
+      @course = FactoryBot.create(:course, name: "英単語テスト", private: false, user_id: @user.id)
+      @another_course = FactoryBot.create(:course, name: "フランス語単語テスト", private: false, user_id: @user.id)
+      @making_course = FactoryBot.create(:course, name: "英単語テスト", private: true, user_id: @user.id)
+    end
+
+    it "nameに検索語句が含まれていて、privateがfalseのcourseが検索結果に含まれる" do
+      expect(Course.search("英単語")).to include(@course)
+    end
+
+    it "nameに検索語句が含まれていても、privateがtrueだと検索結果に含まれない" do
+      expect(Course.search("英単語")).to_not include(@another_course)
+    end
+
+    it "nameに検索語句が含まれないcourseは検索結果に含まれない" do
+      expect(Course.search("英単語")).to_not include(@making_course)
+    end
+  end
 end
