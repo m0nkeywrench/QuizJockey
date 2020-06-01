f9d96fd5f3f8a5b123be071a0f4b9b9677a6d50d
m0nkeywrench <59466775+m0nkeywrench@users.noreply.github.com>
2020-06-01 09:33:30 +0900

    write update action test of questions controller

--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -185,7 +185,7 @@ describe QuestionsController, type: :request do
       sign_in @user
     end
 
-    context "ログインしていないユーザーの場合" do
+    context "ログインしていない場合" do
       it "リダイレクトすること" do
         sign_out @user
         post course_questions_url(course_id: @course.id), params: { question: FactoryBot.attributes_for(:question) }
@@ -267,6 +267,106 @@ describe QuestionsController, type: :request do
 
     context "courseが存在しない場合" do
       subject { -> { post course_questions_url(course_id: @course.id + 1), params: { question: FactoryBot.attributes_for(:question) } } }
+      it { is_expected.to raise_error ActiveRecord::RecordNotFound }
+    end
+  end
+
+  describe "PUT #update" do
+    before do
+      @user = FactoryBot.create(:user)
+      @course = FactoryBot.create(:course, user_id: @user.id)
+      @question = FactoryBot.create(:question, course_id: @course.id)
+      @another_user = FactoryBot.create(:user)
+      sign_in @user
+    end
+
+    context "ログインしていない場合" do
+      it "リダイレクトすること" do
+        sign_out @user
+        put course_question_url(course_id: @course.id, id: @question.id), params: { question: FactoryBot.attributes_for(:question, sentence: "第1問") }
+        expect(response.status).to eq 302
+      end
+
+      it "sentenseが変更されないこと" do
+        sign_out @user
+        expect do
+          put course_question_url(course_id: @course.id, id: @question.id), params: { question: FactoryBot.attributes_for(:question, sentence: "第1問") }
+        end.to_not change(Question.find(@question.id), :sentence)
+      end
+
+      it "new_user_session_urlにリダイレクトすること" do
+        sign_out @user
+        put course_question_url(course_id: @course.id, id: @question.id), params: { question: FactoryBot.attributes_for(:question, sentence: "第1問") }
+        expect(response).to redirect_to new_user_session_url
+      end
+    end
+
+    context "作成者以外がリクエストした場合" do
+      it "リダイレクトすること" do
+        sign_in @another_user
+        put course_question_url(course_id: @course.id, id: @question.id), params: { question: FactoryBot.attributes_for(:question, sentence: "第1問") }
+        expect(response.status).to eq 302
+      end
+  
+      it "sentenceが変更されないこと" do
+        sign_in @another_user
+        expect do
+          put course_question_url(course_id: @course.id, id: @question.id), params: { question: FactoryBot.attributes_for(:question, sentence: "第1問") }
+        end.to_not change(Question.find(@question.id), :sentence)
+      end
+
+      it "course_urlにリダイレクトすること" do
+        sign_in @another_user
+        put course_question_url(course_id: @course.id, id: @question.id), params: { question: FactoryBot.attributes_for(:question, sentence: "第1問") }
+        expect(response).to redirect_to course_url(@course.id)
+      end
+    end
+
+    context "作成者がリクエストした場合" do
+      context "パラメータが妥当な場合" do
+        it "リクエストが成功すること" do
+          put course_question_url(course_id: @course.id, id: @question.id), params: { question: FactoryBot.attributes_for(:question, sentence: "第1問") }
+          expect(response.status).to eq 302
+        end
+
+        it "sentenceが更新されること" do
+          expect do
+            put course_question_url(course_id: @course.id, id: @question.id), params: { question: FactoryBot.attributes_for(:question, sentence: "第1問") }
+          end.to change { Question.find(@question.id).sentence }.from(@question.sentence).to("第1問")
+        end
+
+        it "new_course_questionにリダイレクトすること" do
+          put course_question_url(course_id: @course.id, id: @question.id), params: { question: FactoryBot.attributes_for(:question, sentence: "第1問") }
+          expect(response).to redirect_to new_course_question_path(@course.id)
+        end
+      end
+
+      context "パラメータが不正な場合" do
+        it "リクエストが成功すること" do
+          put course_question_url(course_id: @course.id, id: @question.id), params: { question: FactoryBot.attributes_for(:question, sentence: nil) }
+          expect(response.status).to eq 200
+        end
+
+        it "sentenceが更新されること" do
+          expect do
+            put course_question_url(course_id: @course.id, id: @question.id), params: { question: FactoryBot.attributes_for(:question, sentence: nil) }
+          end.to_not change(Question.find(@question.id), :sentence)
+        end
+
+        it "エラー文言が表示されること" do
+          put course_question_url(course_id: @course.id, id: @question.id), params: { question: FactoryBot.attributes_for(:question, sentence: nil) }
+          expect(response.body).to include("必要な情報が足りません")
+        end
+      end
+    end
+
+    context "courseが存在しない場合" do
+      subject { -> { put course_question_url(course_id: @course.id + 1, id: @question.id), params: { question: FactoryBot.attributes_for(:question, sentence: "第1問") } } }
+      it { is_expected.to raise_error ActiveRecord::RecordNotFound }
+    end
+
+    context "questionが存在しない場合" do
+      subject { -> { put course_question_url(course_id: @course.id, id: @question.id + 1), params: { question: FactoryBot.attributes_for(:question, sentence: "第1問") } } }
       it { is_expected.to raise_error ActiveRecord::RecordNotFound }
     end
   end
